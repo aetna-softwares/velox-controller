4f48a51ff56cef8b43284805340f9f2044964687
Olivier Oriol <olivier.oriol@aetna-softwares.com>
2017-09-04 17:39:21 +0200

    fix export names

--- velox-app-controller.js
+++ velox-app-controller.js
@@ -1,7 +1,7 @@
 ; (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
-            global.VeloxWebView = factory() ;
+            global.VeloxAppController = factory() ;
 }(this, (function () { 'use strict';
     /**
      * Create an unique ID
